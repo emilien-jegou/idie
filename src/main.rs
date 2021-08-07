57e9d115e8bfc030ef57220feebda00b97f2902a
Emilien Jégou <emilien.jegou1@gmail.com>
2021-08-07 23:51:02 +0200

    Initialize warp backend server
    Created the warp server and a basic healthcheck route. Also added a demo
    test for the route.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use warp::Filter;
+
+fn healthcheck() -> impl Filter<Extract = impl warp::Reply> + Clone {
+  warp::path("healthcheck").map(warp::reply)
+}
+
+#[tokio::main]
+async fn main() {
+  let router = healthcheck();
+
+  warp::serve(router).run(([0, 0, 0, 0], 3000)).await;
+}
+
+#[tokio::test]
+async fn test_healthcheck() {
+  let filter = healthcheck();
+
+  // Check if healthcheck route exist.
+  let res = warp::test::request()
+    .path("/healthcheck")
+    .reply(&filter)
+    .await;
+
+  assert_eq!(res.status(), 200);
 }
