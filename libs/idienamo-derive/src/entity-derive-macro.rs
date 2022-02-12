c732c792be684fa898afb678a3522de9ff01cbd8
Emilien Jégou <emilien.jegou1@gmail.com>
2022-02-12 19:46:08 +0100

    feat(idienamo): derive macro for Entity trait

---
+++ libs/idienamo-derive/src/entity-derive-macro.rs
@@ -0,0 +1,19 @@
+use heck::ToSnakeCase;
+use proc_macro::TokenStream;
+use quote::quote;
+
+#[proc_macro_derive(Entity)]
+pub fn entity_macro(input: TokenStream) -> TokenStream {
+  // Parse the string representation
+  let syn::DeriveInput { ident, .. } = syn::parse_macro_input!(input);
+
+  let table_name: String = ident.to_string().to_snake_case();
+  (quote! {
+      impl ::idienamo::Entity for #ident {
+          fn get_table_name() -> &'static str {
+              #table_name
+          }
+      }
+  })
+  .into()
+}
--- libs/idienamo/src/lib.rs
+++ libs/idienamo/src/lib.rs
@@ -6,3 +6,7 @@ pub mod repository;
 pub use connection::DynamoConnection;
 pub use entity::Entity;
 pub use repository::{DynamoRepository, Repository};
+
+pub mod derives {
+  pub use idienamo_derive::*;
+}
