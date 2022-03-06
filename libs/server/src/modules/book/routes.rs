b1d34b8a3c611f95f4e3d5ca236228dd2e8fc3c3
Emilien Jégou <emilien.jegou1@gmail.com>
2022-03-06 20:28:13 +0100

    feat(server): simple ratelimiter middleware
    I want to be able to limit overusage of the server through a simple
    ratelimiter.

--- libs/server/src/modules/book/routes.rs
+++ libs/server/src/modules/book/routes.rs
@@ -1,12 +1,21 @@
 use super::{BookController, BookModule};
+use ratelimit;
 use shaku::HasComponent;
 use std::sync::Arc;
 use warp::{filters::BoxedFilter, Filter, Rejection, Reply};
+use crate::middlewares::rate_limiter::rate_limiter;
 
 /// POST /add_book
 pub fn add_book(module: Arc<BookModule>) -> BoxedFilter<(impl warp::Reply,)> {
+  let limiter = ratelimit::Builder::new()
+    .capacity(50)
+    .quantum(40)
+    .interval(std::time::Duration::new(1, 0))
+    .build();
+
   warp::path!("add_book")
     .and(warp::post())
+    .and(rate_limiter(limiter).untuple_one())
     .and(warp::any().map(move || module.resolve()))
     .and_then(|controller: Arc<dyn BookController>| async move { controller.add_book().await })
     .boxed()
