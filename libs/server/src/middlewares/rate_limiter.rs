1f0df68a1bd13b318f377198a5f636583ebfa456
Emilien Jégou <emilien.jegou1@gmail.com>
2022-03-12 19:42:45 +0100

    feat(server): ratelimiter doesn't need to be multithreaded

--- libs/server/src/middlewares/rate_limiter.rs
+++ libs/server/src/middlewares/rate_limiter.rs
@@ -7,16 +7,16 @@ struct RateLimited;
 
 impl Reject for RateLimited {}
 
-pub fn rate_limiter(mut limiter: ratelimit::Limiter) -> impl Filter<Extract = ((),), Error = Rejection> + Clone {
-  let handle = Arc::new(Mutex::new(limiter.make_handle()));
-
-  std::thread::spawn(move || limiter.run());
+pub fn rate_limiter(
+  limiter: ratelimit::Limiter,
+) -> impl Filter<Extract = ((),), Error = Rejection> + Clone {
+  let handle = Arc::new(Mutex::new(limiter));
 
   let with_handle = warp::any().map(move || handle.clone());
 
   warp::any()
     .and(with_handle)
-    .and_then(|handle: Arc<Mutex<ratelimit::Handle>>| async move {
+    .and_then(|handle: Arc<Mutex<ratelimit::Limiter>>| async move {
       let mut guard = handle.lock().unwrap();
       if (*guard).try_wait().is_err() {
         Err(warp::reject::custom(RateLimited))
